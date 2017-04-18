1f3179da9fd2227e7c90eb0bb506d0b255a67731
Anushree Jangid <anjangid@cisco.com>
2017-04-18 18:45:41 +0530

    Fix setup file

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 set -e
 virtualenv venv-csmpe
 . venv-csmpe/bin/activate
-wget --no-check-certificate https://raw.githubusercontent.com/anushreejangid/csmpe-main/master/requirements.txt
+wget --no-check-certificate https://raw.githubusercontent.com/anushreejangid/csmpe-main/master/requirements.txt -O requirements.txt
 pip install -r requirements.txt
 
