7371c1a84e05f92e284b28bf6702d75863ebf02f
Anushree Jangid <anjangid@cisco.com>
2017-04-18 20:53:38 +0530

    Fix python path in virtualenv

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf venv-csmpe
-virtualenv venv-csmpe && \
+virtualenv -p python-2.7.10 venv-csmpe && \
 source venv-csmpe/bin/activate && \
 wget --no-check-certificate https://raw.githubusercontent.com/anushreejangid/csmpe-main/master/requirements.txt -O requirements.txt && \
 pip install -r requirements.txt
