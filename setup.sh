2f67e39ba22916370743e338314d8a9aba5ad4f3
Anushree Jangid <anjangid@cisco.com>
2017-04-18 19:05:34 +0530

    Add gitlab to csmpe

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
-set -e
-virtualenv venv-csmpe
-. venv-csmpe/bin/activate
-wget --no-check-certificate https://raw.githubusercontent.com/anushreejangid/csmpe-main/master/requirements.txt -O requirements.txt
+rm -rf venv-csmpe
+virtualenv venv-csmpe && \
+( . venv-csmpe/bin/activate ) && \
+(
+if [[ "CSM$CSMPE_GITHUB" == "CSM" ]]; then
+    wget --no-check-certificate https://raw.githubusercontent.com/anushreejangid/csmpe-main/master/requirements.txt -O requirements.txt
+else
+    wget --no-check-certificate http://gitlab.cisco.com/anjangid/csmpe-main/raw/master/setup.sh -O requirement.txt
+fi
+) && \
 pip install -r requirements.txt
 
