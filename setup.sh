904136b2776f8ad639f0aed7c407fb4e4035c0c0
Anushree Jangid <anjangid@cisco.com>
2017-04-18 19:14:53 +0530

    Fix setup file

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 rm -rf venv-csmpe
 virtualenv venv-csmpe && \
-( . venv-csmpe/bin/activate ) && \
+source venv-csmpe/bin/activate && \
 (
 if [[ "CSM$CSMPE_GITHUB" == "CSM" ]]; then
-    wget --no-check-certificate https://raw.githubusercontent.com/anushreejangid/csmpe-main/master/requirements.txt -O requirements.txt
-else
     wget --no-check-certificate http://gitlab.cisco.com/anjangid/csmpe-main/raw/master/setup.sh -O requirement.txt
+else
+    wget --no-check-certificate https://raw.githubusercontent.com/anushreejangid/csmpe-main/master/requirements.txt -O requirements.txt
 fi
 ) && \
 pip install -r requirements.txt
